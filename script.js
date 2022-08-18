e282266cf58286b6e3c173ca4df5b8d1c9db07be
Tania Avila <tanigavila@gmail.com>
2022-08-17 21:20:55 -0300

    first commit

--- script.js
+++ script.js
@@ -1,35 +1,20 @@
 // DATOS: Tania Avila - 827625
 
-/*
-Cree una función llamada  function gastoConIva() que acepte un número y devuelva ese número más el 21% que es el valor del IVA.
-Luego crea una function map() que tome dos entradas o parámetros:
-un array de como valores números. 
-una función callback: esta función se aplica a cada elemento del array (dentro de la función map().
-Haga que su function map() devuelva una nueva matriz llena de números que son el resultado de usar la función callback en cada elemento de la matriz de entrada.
-*/
+let form = document.querySelector("form");
+let input = document.querySelector("input");
 
+form.addEventListener("submit",(e)=>{
+  e.preventDefault()
+  let valor = input.value
+  
+  let regexp = /^(?=(?:.*[A-Z]){1})(?=(?:.*[a-z]){1})\S{12,}$/
+  
 
-function gastoConIva (num) {
-  precio = num*1.21
-    return precio;
-}
+  if (regexp.test(valor)) {
+    console.log("Válido");
+  } else {
+    console.log("No Válido");
+  }
 
-
-
-function map (array, callback) {
-   
-    let nuevoresult = array.map(callback);
-
-
-    for ( var i = 0 ; i < array.length ; i++ ) {
-
-      console.log (array);
-      console.log ("El gasto con IVA del item " + ( i + 1 ) + " es: " + gastoConIva(array[i]));
-
-    }
-
-return nuevoresult;
-}
-
-console.log (map( [100,200,300,400], gastoConIva) );
+} )
 
